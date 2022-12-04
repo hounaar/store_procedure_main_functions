26799b9ad21ce022554b2b3f67c0aa2f4f2a2651
Hounaar <95884627+hounaar@users.noreply.github.com>
2022-12-04 15:23:32 +0330

    Create insert.php

---
+++ src/insert.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="/test/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script> 
+    <title>Store procedure project</title>
+</head>
+<body>
+
+<form method="POST" class="animate__animated animate__fadeInUp insert mr-5 mt-5 mb-5 ml-5" id="insert">
+        <input type="text" class="form-control" name="id" placeholder="Enter a id"><br/>    
+        <input type="text" class="form-control" name="name" placeholder="Enter a name"><br/>
+        <input type="text" class="form-control" name="username" placeholder="Enter a username"><br/>
+        <input type="text" class="form-control" name="email" placeholder="Enter a email"><br/>
+        <button type="submit" name="insertsubmit" class="insertsubmit btn btn-outline-success">Insert</button>
+        <div class="col error-text mt-5"></div>
+    </form>
+    <br/>    <br/>
+    <br/>
+    <br/>
+    <script type="text/javascript" src="/test/insert/js/insert.js"></script>
+</body>
+</html>
+  
+
+
